2660d82ce16155f85e88c0b35078d475a55e81d1
Ego <88577304+ego1337@users.noreply.github.com>
2022-12-05 04:13:49 -0500

    Create chams.lua

---
+++ chams.lua
@@ -0,0 +1,8 @@
+
+_G.FriendColor = Color3.fromRGB(255, 255, 255)
+_G.EnemyColor = Color3.fromRGB(255, 131, 244)
+_G.UseTeamColor = false
+
+--------------------------------------------------------------------
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zeroisswag/universal-esp/main/esp.lua"))()
